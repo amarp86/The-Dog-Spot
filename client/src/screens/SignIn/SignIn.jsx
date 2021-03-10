80a4f7393868408c008329660af8b6d2499af214
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-10 14:24:00 -0500

    fixed default adoption status

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./SignIn.css";
 import { signIn } from "../../services/users";
 import { useHistory } from "react-router-dom";
+import Layout from "../../components/shared/Layout/Layout";
 
 const SignIn = (props) => {
   const history = useHistory();
