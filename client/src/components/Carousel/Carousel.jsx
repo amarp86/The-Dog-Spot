e8d33a4d3a446bd01ad5a5e28e0bce5736e1433e
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-03-10 17:11:14 -0500

    ignore

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -5,37 +5,6 @@ import { useEffect, useState } from "react";
 const Carousel = (props) => {
   const [adoptedDogs, setAdoptedDogs] = useState([]);
 
-<<<<<<< HEAD
-
-
-const Carousel = () => {
-  const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
-  };
-  
-  const items = [
-    <div className="item" data-value="1">
-      1
-    </div>,
-    <div className="item" data-value="2">
-      2
-    </div>,
-    <div className="item" data-value="3">
-      3
-    </div>,
-    <div className="item" data-value="4">
-      4
-    </div>,
-    <div className="item" data-value="5">
-      5
-    </div>,
-  ];
-  return (
-    <div>
-       <AliceCarousel mouseTracking items={items} responsive={responsive} />
-=======
   useEffect(() => {
     const fetchDogs = async () => {
       const dogs = await getAdoptedDogs();
@@ -58,7 +27,6 @@ const Carousel = () => {
   return (
     <div>
       <AliceCarousel mouseTracking items={items} responsive={responsive} />
->>>>>>> fa60a32471da7948fc777d2d663c07ecb8abc4c7
     </div>
   );
 };
