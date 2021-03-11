f777650bafd0e066018bc6a6f73834725287e922
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-11 14:08:50 -0500

    footer image fixed

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import "./Footer.css";
+import linkedin from "../../../images/linkedinlogo.png";
 
 function Footer(props) {
   return (
@@ -7,31 +8,19 @@ function Footer(props) {
         <div className="hanna">
           <a href="https://www.linkedin.com/in/hanna-boorom/">
             Hanna Boorom
-            <img
-              className="linkedin-image"
-              src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1615482816~hmac=5b2dbec5d95d006134bc6aef63646883"
-              alt="linkedin"
-            />
+            <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
         </div>
         <div className="jason">
           <a href="https://www.linkedin.com/in/jason-quizhpi-30a41b169/">
             Jason Quizhpi
-            <img
-              className="linkedin-image"
-              src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1615482816~hmac=5b2dbec5d95d006134bc6aef63646883"
-              alt="linkedin"
-            />
+            <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
         </div>
         <div className="amar">
           <a href="https://www.linkedin.com/in/amarp86/">
             Amar Patel
-            <img
-              className="linkedin-image"
-              src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1615482816~hmac=5b2dbec5d95d006134bc6aef63646883"
-              alt="linkedin"
-            />
+            <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
         </div>
       </div>
