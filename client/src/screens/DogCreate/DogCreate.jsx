85661b6fe1705b5f8b004655edf677b73dc63ec5
Hanna <Hanna@Taylors-MacBook-Pro-2.local>
2021-03-11 17:40:59 -0600

    beautified add form

--- client/src/screens/DogCreate/DogCreate.jsx
+++ client/src/screens/DogCreate/DogCreate.jsx
@@ -57,66 +57,69 @@ const DogCreate = (props) => {
   };
 
   return (
-    <form className="create-form" onSubmit={handleSubmit}>
-      <input
-        className="input-name"
-        placeholder="Name"
-        value={dog.name}
-        name="name"
-        required
-        autoFocus
-        onChange={handleChange}
-      />
-      <input
-        className="input-age"
-        placeholder="age"
-        value={dog.age}
-        name="age"
-        required
-        onChange={handleChange}
-      />
-      <textarea
-        className="textarea-description"
-        rows={10}
-        placeholder="Description"
-        value={dog.description}
-        name="description"
-        required
-        onChange={handleChange}
-      />
-      <input
-        className="input-image-link"
-        placeholder="Image Link"
-        value={dog.images}
-        name="images"
-        required
-        onChange={handleChange}
-      />
-      <input
-        className="input-location"
-        placeholder="Dog Location"
-        value={dog.location}
-        name="location"
-        required
-        onChange={handleChange}
-      />
+    <div className="create-container">
+      <h3>List a Dog for Adoption</h3>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          className="input-name"
+          placeholder="Name"
+          value={dog.name}
+          name="name"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <input
+          className="input-age"
+          placeholder="Age"
+          value={dog.age}
+          name="age"
+          required
+          onChange={handleChange}
+        />
+        <textarea
+          className="textarea-description"
+          rows={10}
+          placeholder="Description"
+          value={dog.description}
+          name="description"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-image-link"
+          placeholder="Image Link"
+          value={dog.images}
+          name="images"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-location"
+          placeholder="Dog Location"
+          value={dog.location}
+          name="location"
+          required
+          onChange={handleChange}
+        />
 
-      <select
-        className="breed-select"
-        value={dog.breed}
-        name="breed"
-        required
-        onChange={handleChange}
-      >
-        {allBreeds.map((dog, index) => (
-          <option key={index}>{titleCase(dog.breedName)}</option>
-        ))}
-      </select>
+        <select
+          className="breed-select"
+          value={dog.breed}
+          name="breed"
+          required
+          onChange={handleChange}
+        >
+          {allBreeds.map((dog, index) => (
+            <option key={index}>{titleCase(dog.breedName)}</option>
+          ))}
+        </select>
 
-      <button type="submit" className="submit-button">
-        Fetch
-      </button>
-    </form>
+        <button type="submit" className="submit-button">
+          Fetch
+        </button>
+      </form>
+    </div>
   );
 };
 
