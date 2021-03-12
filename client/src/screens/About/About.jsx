dbc7e04e75466e6605cefd6c086a6f9447d119ae
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-03-11 23:03:33 -0500

    updated about text

--- client/src/screens/About/About.jsx
+++ client/src/screens/About/About.jsx
@@ -12,22 +12,23 @@ function About(props) {
         />
         <h1>Who We Are.</h1>
       </div>
-      
+
       <div className="about-us">
         <p>
-        The Dog Spot is and not-for-profit organization based in New Jersey and
-        currently expanding its company to new heights. Our organization was
-        found by three individuals, Amar, Hanna, and Jason whose beliefs are
-        that shelter dogs deserve respect and be place in loving homes. We team
-        up with other shelter organizations through programs that promote
-        adopting shelter dogs and having them post their dogs on our site. We
-        have succesfully maintain a strong presence and have proudly gained over
-        5 million supporters. We want these dogs to get a second chance in
-        happiness again.
+          The Dog Spot is and not-for-profit organization based in San Diego,
+          California and currently expanding to new heights. Our
+          organization was found by three individuals, Amar, Hanna, and Jason
+          whose beliefs are that shelter dogs deserve a second chance and be place in
+          loving homes. We team up with other local shelter organizations promoting their adopting dogs and through
+          programs with animal placement organizations. We are currently trying to reach out to more shelters throughout the United States. Our facility responds to
+          calls from the public to help keep our community safe and rescue
+          animals in need rescued. We have succesfully maintain a strong presence and
+          have proudly gained over a million supporters. We want these dogs to get a second chance in
+          happiness again.
         </p>
       </div>
-      
-      <Container/>
+
+      <Container />
     </div>
   );
 }
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,31 +1,35 @@
 import Carousel from "../../components/Carousel/Carousel";
-import "./Home.css"
+import "./Home.css";
 
 import Container from "../../components/shared/Container/Container";
 
 function Home(props) {
- 
   return (
     <div className="home-area">
       <div className="home-banner-image">
-      
-        <img className="banner"
+        <img
+          className="banner"
           src="https://images.unsplash.com/photo-1601758176175-45914394491c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
           alt="banner"
         />
-        <div className="headliner">Every Dog Deserves a Home.</div> 
-    
+        <div className="headliner">Every Dog Deserves a Home.</div>
       </div>
       <div className="brand-statement">
-        <div className="statement">The Dog Spot is an online organization based in San Diego, California where we help animal shelters from the USA adopt their dog. Feel free to check out the available dogs or add a dog ready to be adopted to our site.</div>
+        <div className="statement">
+          The Dog Spot is an online organization based in San Diego, California
+          where our mission is to give every dog a second chance in life.
+          We strive to find loving homes for homeless and abandoned dogs
+          both by promoting adopting animals from local animal shelters and by
+          partnering with animal placement organizations. Feel free to check out the available dogs or add a dog ready to be adopted to our
+          site.
+        </div>
       </div>
       <div className="carousel">
-       <div className="adopted">Adopted Dogs</div> 
+        <div className="adopted">Adopted Dogs</div>
         <Carousel />
       </div>
-       <Container/>
-      </div>
-    
+      <Container />
+    </div>
   );
 }
 
