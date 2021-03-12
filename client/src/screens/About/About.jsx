3e70faeb38577dffcf7c71d9798f900f0fbda8f5
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-03-11 22:06:41 -0500

    updated css

--- client/src/screens/About/About.jsx
+++ client/src/screens/About/About.jsx
@@ -1,24 +1,33 @@
+import Container from "../../components/shared/Container/Container";
+import "./About.css";
 
 function About(props) {
   return (
-    <div>
-      <div className="about-banner-image">
-        
+    <div className="about">
+      <div className="about-banner">
         <img
+          className="banner-image"
           src="http://www.aspca.org/sites/default/files/nyc-adoption-center-facebook.jpg"
           alt="banner"
         />
-         <h1>Who We Are.</h1>
+        <h1>Who We Are.</h1>
       </div>
+      
       <div className="about-us">
-        The Dog Spot is and not-for-profit organization based in New Jersey and currently expanding its company to new heights. Our organization was found by three individuals, Amar, Hanna, and Jason whose beliefs are that shelter dogs deserve respect and be place in loving homes. We team up with other shelter organizations through programs that promote adopting shelter dogs and having them post their dogs on our site. We have succesfully maintain a strong presence and have proudly gained over 5 million supporters. We want these dogs to get a second chance in happiness again. 
-        </div>
-      <div className="container">
-        You can make a difference:
-         <img className="icon" alt="icon"/>
-         <img className="icon" alt="icon"/>
+        <p>
+        The Dog Spot is and not-for-profit organization based in New Jersey and
+        currently expanding its company to new heights. Our organization was
+        found by three individuals, Amar, Hanna, and Jason whose beliefs are
+        that shelter dogs deserve respect and be place in loving homes. We team
+        up with other shelter organizations through programs that promote
+        adopting shelter dogs and having them post their dogs on our site. We
+        have succesfully maintain a strong presence and have proudly gained over
+        5 million supporters. We want these dogs to get a second chance in
+        happiness again.
+        </p>
       </div>
-
+      
+      <Container/>
     </div>
   );
 }
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,13 +1,10 @@
 import Carousel from "../../components/Carousel/Carousel";
 import "./Home.css"
-import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDog } from '@fortawesome/free-solid-svg-icons'
-import { faDonate} from '@fortawesome/free-solid-svg-icons'
+
+import Container from "../../components/shared/Container/Container";
 
 function Home(props) {
-  const element = <FontAwesomeIcon icon={faDog} size="6x" />
-  const donate = <FontAwesomeIcon icon={faDonate}  size="6x" />
+ 
   return (
     <div className="home-area">
       <div className="home-banner-image">
@@ -26,31 +23,9 @@ function Home(props) {
        <div className="adopted">Adopted Dogs</div> 
         <Carousel />
       </div>
-
-      <div className="make-a-difference">
-        <img className="banner-two" alt="baner"src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2020/04/shelter-dog-cropped-1.jpg"/>
-        <div className="title">You can make a difference</div>
-        
-        <div className="home-links">
-          <div className="container-link">
-          <Link  to="/dogs">
-         
-            <div className="adopt">Adopt</div>
-            
-            <div className="dog-icon">{element}</div>
-            </Link ></div>
-          <div className="container-link">
-          <Link to="/donate">
-          
-          <div className="adopt"> Donate</div>  
-              
-          <div className="donate-icon">{donate}</div>
-            </Link>
-            </div>
-        </div>
-         
+       <Container/>
       </div>
-    </div>
+    
   );
 }
 
