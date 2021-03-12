b0317de347483cc01c27c914c7b7d7f01083a55f
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-12 09:13:43 -0500

    fixed footer error

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -6,19 +6,31 @@ function Footer(props) {
     <div className="footer-area">
       <div className="contributors">
         <div className="hanna">
-          <a  href="https://www.linkedin.com/in/hanna-boorom/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/hanna-boorom/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Hanna Boorom
             <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
         </div>
         <div className="jason">
-          <a  href="https://www.linkedin.com/in/jason-quizhpi-30a41b169/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/jason-quizhpi-30a41b169/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Jason Quizhpi
             <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
         </div>
         <div className="amar">
-          <a   href="https://www.linkedin.com/in/amarp86/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/amarp86/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Amar Patel
             <img className="linkedin-image" src={linkedin} alt="linkedin" />
           </a>
