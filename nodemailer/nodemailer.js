e2730231edd9e886311ca7a4a0a7d2de4e772f5a
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-03-15 11:41:17 -0400

    ternary welcome message

--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -16,7 +16,20 @@ async function sendEmail(email) {
     to: `${email}`,
     subject: "Welcome to The Dog Spot",
     text: "Thank You for Signing Up at The Dog Spot!",
-    html: "<b>Hello world?</b>",
+    html: `<html>
+    <head>
+      <meta charset="utf-8">
+      <style amp4email-boilerplate>body{visibility:hidden}</style>
+      <script async src="https://cdn.ampproject.org/v0.js"></script>
+      <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+    </head>
+    <body>
+      <h1>Thank you for signing-up at The Dog Spot!"</h1>
+      <img src="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80" alt="logo"/>
+      <h2>Sincerely, Hanna, Jason, Amar </h2>
+
+    </body>
+  </html>`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
